a17c1860b54b93b34057184c9b9787e3eef4732f
Gareth Miskimmin <gareth.miskimmin@kainos.com>
2021-12-13 15:41:02 +0000

    Added testing

---
+++ src/test/java/service/JobRolesServiceTest.java
@@ -0,0 +1,43 @@
+package service;
+
+import com.kainos.ea.data.JobRolesDAO;
+import com.kainos.ea.model.JobRole;
+import com.kainos.ea.service.JobRolesService;
+import com.kainos.ea.util.DatabaseConnector;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JobRolesServiceTest {
+    @Test
+    void testServiceGetRolesCallsDAO() throws SQLException {
+        Connection connection = Mockito.mock(Connection.class);
+        DatabaseConnector connector = Mockito.mock(DatabaseConnector.class);
+        Mockito.when(connector.getConnection()).thenReturn(connection);
+
+        JobRole jobRole1 = new JobRole(1, "Dev");
+        JobRole jobRole2 = new JobRole(2, "Tester");
+
+        List<JobRole> jobRoles = new ArrayList<>();
+        jobRoles.add(jobRole1);
+        jobRoles.add(jobRole2);
+
+        JobRolesDAO jobRolesDAO = Mockito.mock(JobRolesDAO.class);
+        Mockito.when(jobRolesDAO.getJobRolesFromDatabase(connection)).thenReturn(jobRoles);
+
+        JobRolesService jobRolesService = new JobRolesService(jobRolesDAO, connector);
+        List<JobRole> returnedList = jobRolesService.getJobRoles();
+
+        Mockito.verify(connector).getConnection();
+        Mockito.verify(jobRolesDAO).getJobRolesFromDatabase(connection);
+
+        assertEquals(jobRoles, returnedList);
+
+    }
+}
