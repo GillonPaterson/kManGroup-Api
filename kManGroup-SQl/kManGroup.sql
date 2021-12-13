7b928e7794c8fea65dc62a23fe183e1fc9b95748
Gareth Miskimmin <92785721+garethmiskimmin@users.noreply.github.com>
2021-12-13 14:11:41 +0000

    Update kManGroup.sql
    is this it

--- kManGroup-SQl/kManGroup.sql
+++ kManGroup-SQl/kManGroup.sql
@@ -3,5 +3,6 @@ create database kManGroup_gillon;
 use kManGroup_gillon;
 
 create table jobRoles (
-employeeID int auto_increment unique,
-employeeRole varchar(150));
+employeeID int auto_increment NOT NULL,
+employeeRole varchar(150)) NOT NULL,
+CONSTRAINT pk_Employee_ID PRIMARY KEY (employeeID);
