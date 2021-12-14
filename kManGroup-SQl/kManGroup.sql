c5407da69724a201cc887dafa3d50e7878c8a71a
Gareth Miskimmin <92785721+garethmiskimmin@users.noreply.github.com>
2021-12-14 10:11:53 +0000

    Update kManGroup.sql

--- kManGroup-SQl/kManGroup.sql
+++ kManGroup-SQl/kManGroup.sql
@@ -1,7 +1,7 @@
+DROP DATABASE kManGroup_gillon
+CREATE DATABASE kManGroup_gillon
 
 use kManGroup_gillon;
-
-drop table jobRoles;
 
 create table jobRoles (
 jobRoleID int auto_increment NOT NULL,
@@ -11,7 +11,5 @@ CONSTRAINT pk_Job_Role_ID PRIMARY KEY (jobRoleID));
 insert into jobRoles(jobRole) values('Software Engineer');
 insert into jobRoles(jobRole) values('Front-End Engineer');
 insert into jobRoles(jobRole) values('Test Engineer');
-
-select * from jobRoles;
 
 
