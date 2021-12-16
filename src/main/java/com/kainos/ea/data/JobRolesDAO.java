33f92e85b4793dd34f90e58e735685928663697d
Nikkala Pokojski <nikkala.pokojski@kainos.com>
2021-12-16 17:29:29 +0000

    US008-Backend

--- src/main/java/com/kainos/ea/data/JobRolesDAO.java
+++ src/main/java/com/kainos/ea/data/JobRolesDAO.java
@@ -59,13 +59,13 @@ public class JobRolesDAO {
 
     public List<JobTraining> getJobTrainingFromDatabase(Connection connection, String bandLevel) throws SQLException {
         List<JobTraining> training = new ArrayList<>();
-        String query = "SELECT bandLevels.jobBandLevel, training.trainingLink FROM bandLevels inner join bandLevelsTraining using(jobBandLevelID) inner join training using(trainingID) WHERE jobBandLevel = ?";
+        String query = "SELECT bandLevels.jobBandLevel, training.trainingName, training.trainingLink FROM bandLevels inner join bandLevelsTraining using(jobBandLevelID) inner join training using(trainingID) WHERE jobBandLevel = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         preparedStatement.setString(1, bandLevel);
 
         ResultSet rs = preparedStatement.executeQuery();
         while (rs.next()){
-            JobTraining jobTraining = new JobTraining(rs.getString("jobBandLevel"), rs.getString("trainingLink"));
+            JobTraining jobTraining = new JobTraining(rs.getString("jobBandLevel"), rs.getString("trainingName"), rs.getString("trainingLink"));
             training.add(jobTraining);
         }
         return training;
--- src/main/java/com/kainos/ea/model/JobTraining.java
+++ src/main/java/com/kainos/ea/model/JobTraining.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class JobTraining {
     private String jobBandLevel;
+    private String trainingName;
     private String trainingLink;
 
     @JsonCreator
-    public JobTraining(@JsonProperty("jobBandLevel") String JobBandLevel, @JsonProperty("trainingLink") String TrainingLink) {
+    public JobTraining(@JsonProperty("jobBandLevel") String JobBandLevel, @JsonProperty("trainingName") String TrainingName, @JsonProperty("trainingLink") String TrainingLink) {
         this.setJobBandLevel(JobBandLevel);
+        this.setTrainingName(TrainingName);
         this.setTrainingLink(TrainingLink);
     }
 
@@ -27,5 +29,13 @@ public class JobTraining {
 
     public void setTrainingLink(String trainingLink) {
         this.trainingLink = trainingLink;
+    }
+
+    public String getTrainingName() {
+        return trainingName;
+    }
+
+    public void setTrainingName(String trainingName) {
+        this.trainingName = trainingName;
     }
 }
