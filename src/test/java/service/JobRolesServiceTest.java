57f36f2147c77d415729b70335483c287785674e
GillonPaterson <gillon.paterson@kainos.com>
2021-12-17 10:11:52 +0000

    USC-005-API-testing

--- src/test/java/service/JobRolesServiceTest.java
+++ src/test/java/service/JobRolesServiceTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import com.kainos.ea.data.JobRolesDAO;
+import com.kainos.ea.model.Competency;
 import com.kainos.ea.model.JobRole;
 import com.kainos.ea.model.JobSpecModel;
 import com.kainos.ea.service.JobRolesService;
@@ -8,6 +9,7 @@ import com.kainos.ea.util.DatabaseConnector;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import javax.annotation.meta.When;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -59,5 +61,36 @@ class JobRolesServiceTest {
         Mockito.verify(jobRolesDAO).getJobSpecFromDatabase(connection, 1);
 
         assertEquals(jobSpecModel, returnedModel);
+    }
+
+    @Test
+    void testServicegetComp() throws SQLException {
+
+        Connection connection = Mockito.mock(Connection.class);
+        DatabaseConnector connector = Mockito.mock(DatabaseConnector.class);
+        Mockito.when(connector.getConnection()).thenReturn(connection);
+
+        Competency competencyModel = new Competency("Associate",
+                "Reflects on owninteractions with a wide and diverse range of individuals andgroups from within and beyond immediate service/organisation.Challenges and refreshes own values, beliefs, leadership styles and approaches. Overtly role models the giving and receiving of feedback.",
+                "'Successfully manages a range of personal and organisational demandsand pressures. Demonstrates tenacity and resilience. Overcomes setbackswhere goals cannot be achieved and quickly refocuses. Is visible andaccessible to others.'",
+                "Develops through systematically scanningthe external environment and exploring leading edge thinking and best practice.Applies learning to build and refresh the business. Treats challenge as a positive forcefor improvement.",
+                null,
+                "stage 3");
+
+        Competency competencyModelService = new Competency("Associate","Reflects on owninteractions with a wide and diverse range of individuals andgroups from within and beyond immediate service/organisation.Challenges and refreshes own values, beliefs, leadership styles and approaches. Overtly role models the giving and receiving of feedback.","'Successfully manages a range of personal and organisational demandsand pressures. Demonstrates tenacity and resilience. Overcomes setbackswhere goals cannot be achieved and quickly refocuses. Is visible andaccessible to others.'","Develops through systematically scanningthe external environment and exploring leading edge thinking and best practice.Applies learning to build and refresh the business. Treats challenge as a positive forcefor improvement.");
+
+
+        JobRolesDAO jobRolesDAO = Mockito.mock(JobRolesDAO.class);
+        Mockito.when(jobRolesDAO.getJobCompFromDatabase(connection, 1)).thenReturn(competencyModel);
+
+        JobRolesService jobRolesService = new JobRolesService(jobRolesDAO, connector);
+        Competency returnedModel = jobRolesService.getComp(1);
+
+        Mockito.verify(connector).getConnection();
+        Mockito.verify(jobRolesDAO).getJobCompFromDatabase(connection, 1);
+        assertEquals(competencyModelService.getBandLevel(), returnedModel.getBandLevel());
+        assertEquals(competencyModelService.getCompetencyStage1(), returnedModel.getCompetencyStage1());
+        assertEquals(competencyModelService.getCompetencyStage2(), returnedModel.getCompetencyStage2());
+        assertEquals(competencyModelService.getCompetencyStage3(), returnedModel.getCompetencyStage3());
     }
 }
